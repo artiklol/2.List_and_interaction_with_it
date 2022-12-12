baf6453ea2240ed4aed212c514225da9d89a3ce3
Artem Sulzhenko <artiklol2012@gmail.com>
2022-12-12 15:16:28 +0300

    ADDED project

---
+++ task_2/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  task_2
+//
+//  Created by Artem Sulzhenko on 12.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+
+}
+
